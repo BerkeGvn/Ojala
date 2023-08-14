a028f811cf874a8c84a2cce8ebcf3beb1075276e
Berke <berkegvn@gmail.com>
2023-08-14 19:28:55 +0900

    navbar  + header

---
+++ src/components/home/HomepageHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+    <h1 class="header-h1">OJALA</h1>
+    <div class="header-sub">
+      <h2 class="header-sub-h2">CREATING COMFORT, UNITING STYLE: YOUR HOME, ELEVATED.</h2>
+      <button>Explore now!</button>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+@import '../../assets/style/variables.scss';
+.header {
+  background-image: url(../../assets/images/home-header-small.jpg);
+  background-position: center;
+  background-size: cover;
+  height: 90vh;
+  color: var(--oj-text-color-0);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2rem;
+  &-h1 {
+    text-transform: uppercase;
+    font-size: var(--oj-h0-size);
+    align-self: center;
+  }
+  &-sub {
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+    padding-bottom: 8rem;
+    &-h2 {
+      text-transform: uppercase;
+      font-size: var(--oj-h7-size);
+      width: 80%;
+    }
+    & button {
+      align-self: center;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>hello</div>
+  <HomepageHeader></HomepageHeader>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomepageHeader from '../components/home/HomepageHeader.vue'
+</script>
 
 <style scoped></style>
