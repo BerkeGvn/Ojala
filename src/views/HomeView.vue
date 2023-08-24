8b9a086a4f9e036823b42b4888c596d45574286f
Berke <berkegvn@gmail.com>
2023-08-24 19:36:37 +0900

    featured-products + categories

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
   <HomepageAbout></HomepageAbout>
   <HomepageTodaysProduct></HomepageTodaysProduct>
   <HomepageFeatured></HomepageFeatured>
+  <HomepageCategories></HomepageCategories>
 </template>
 
 <script setup lang="ts">
@@ -10,6 +11,7 @@ import HomepageHeader from '@/components/home/HomepageHeader.vue'
 import HomepageAbout from '@/components/home/HomepageAbout.vue'
 import HomepageTodaysProduct from '@/components/home/HomepageTodaysProduct.vue'
 import HomepageFeatured from '@/components/home/HomepageFeatured.vue'
+import HomepageCategories from '@/components/home/HomepageCategories.vue'
 </script>
 
 <style scoped></style>
