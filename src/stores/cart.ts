2eb9550bae58c45aa3022088732c1d776c6c300c
Berke <berkegvn@gmail.com>
2023-09-08 23:56:58 +0900

    cart

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,9 +1,22 @@
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import type { Product } from '../types/types'
 
 export const useCartStore = defineStore('cart', () => {
-  const cart = ref<Product>({ name: 'Ojala Chair', price: 99.0, id: 'c2', images: ['asd', 'sf'] })
+  const cart = ref<Product[]>([])
 
-  return { cart }
+  const getAllProducts = computed(() => {
+    return cart.value
+  })
+
+  const numberOfProductsInCart = computed(() => {
+    return cart.value.length
+  })
+
+  function addToCart(product: Product) {
+    cart.value.push(product)
+    console.log(cart.value)
+  }
+
+  return { cart, numberOfProductsInCart, getAllProducts, addToCart }
 })
